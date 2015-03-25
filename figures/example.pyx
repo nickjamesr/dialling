d3cd56e0f3b9677decc16d0ebccef8c97149c2c2
Nick Russell <nick.russell@bristol.ac.uk>
2015-03-25 16:07:07 +0000

    Complete example figure
    Add graphs of pdfs
    Expand to 6x6
    Add small MZI and BS for key
    Edit caption and text
    Begin section on qubits

--- figures/example.pyx
+++ figures/example.pyx
@@ -1,17 +1,108 @@
 set terminal pdf
 set output "example.pdf"
 
-fig_w=12
+set style 1 colour cyan
+set style 2 colour yellow
+set style 3 colour blue
+set style 4 colour green
+set style 5 colour red
+
+fig_w=18
 fig_m=fig_w/100.
+plot_w=45*fig_m
+plot_h=20*fig_m
+
 
 set multiplot
   set nodisplay
-  eps "example.eps" at 0,0 width 95*fig_m
+  eps "example.eps" at 10*fig_m,plot_h+5*fig_m width 80*fig_m
+  eps "beamsplitter.eps" at 35*fig_m,plot_h-5*fig_m width 10*fig_m
+  eps "mzi.eps" at 70*fig_m,plot_h-5*fig_m width 25*fig_m
 
-  # Reflectivity labels
+  # Beamsplitter reflectivities
+  set size plot_w
+  set size ratio plot_h/plot_w
+  set axis x arrow nomirror
+  set axis y arrow nomirror
+  set xrange [0:1.1]
+  set yrange [0:6.0]
+  set xtics out 1
+  set ytics out 0,5
+  set mytics 1
+  set origin 5*fig_m,0
+  plot
+  set axis x invisible
+  set axis y invisible
+  set xrange [0:1]
+  set yrange [0:5]
+  set size plot_w*11/12.
+  set size ratio (plot_h*5/6.)/(plot_w*11/12.)
+  plot 1 notitle with style 1,\
+    2*(1-x) notitle with style 2,\
+    3*(1-x)**2 notitle with style 3,\
+    4*(1-x)**3 notitle with style 4,\
+    5*(1-x)**4 notitle with style 5
 
-  # Graph labels
-  
+  # MZI phases
+  set size plot_w
+  set size ratio plot_h/plot_w
+  set axis x visible arrow nomirror
+  set axis y visible arrow nomirror
+  set xrange [0:1.1*pi]
+  set yrange [0:1.2]
+  set xtics out ("\(\pi\)" pi)
+  set ytics out 0,1,5
+  set mytics 0.2
+  set origin 55*fig_m,0
+  plot
+  set axis x invisible
+  set axis y invisible
+  set xrange [0:pi]
+  set yrange [0:1]
+  set size plot_w*11/12.
+  set size ratio (plot_h*5/6.)/(plot_w*11/12.)
+  plot cos(0.5*x)*sin(0.5*x) notitle with style 1,\
+    2*cos(0.5*x)**3*sin(0.5*x) notitle with style 2,\
+    3*cos(0.5*x)**5*sin(0.5*x) notitle with style 3,\
+    4*cos(0.5*x)**7*sin(0.5*x) notitle with style 4,\
+    5*cos(0.5*x)**9*sin(0.5*x) notitle with style 5
+
+  # Axis labels
+  text "\(r\)" at 49*fig_m,-3*fig_m
+  text "\(\theta\)" at 99*fig_m,-3*fig_m
+  text "\(\mathcal{P}\left(r\right)\)" at 6*fig_m,plot_h-1*fig_m
+  text "\(\mathcal{P}\left(\theta\right)\)" at 56*fig_m,plot_h-1*fig_m
+
+  # Component labels
+  # In graphs
+  text "\(r\)" at 40*fig_m,plot_h+2*fig_m
+  text "\(\theta\)" at 81*fig_m,plot_h+2*fig_m
+  # In Reck scheme
+  text "\(r_{6,1}\)" at 49*fig_m,49*fig_m
+  text "\(r_{6,2}\)" at 57*fig_m,45*fig_m
+  text "\(r_{6,3}\)" at 67*fig_m,40*fig_m
+  text "\(r_{6,4}\)" at 75*fig_m,36*fig_m
+  text "\(r_{6,5}\)" at 84*fig_m,31*fig_m
+
+  text "\(r_{5,1}\)" at 40*fig_m,45*fig_m
+  text "\(r_{5,2}\)" at 49*fig_m,40*fig_m
+  text "\(r_{5,3}\)" at 57*fig_m,36*fig_m
+  text "\(r_{5,4}\)" at 67*fig_m,31*fig_m
+
+  text "\(r_{4,1}\)" at 31*fig_m,40*fig_m
+  text "\(r_{4,2}\)" at 40*fig_m,36*fig_m
+  text "\(r_{4,3}\)" at 49*fig_m,31*fig_m
+
+  text "\(r_{3,1}\)" at 22*fig_m,36*fig_m
+  text "\(r_{3,2}\)" at 31*fig_m,31*fig_m
+
+  text "\(r_{2,1}\)" at 13*fig_m,31*fig_m
+
+  # Subfigure labels
+  text "(a)" at 4*fig_m,48*fig_m
+  text "(b)" at 4*fig_m,22*fig_m
+  text "(c)" at 54*fig_m,22*fig_m
+
   set display
   refresh
 unset multiplot
