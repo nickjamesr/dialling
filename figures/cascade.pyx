969cfb32de05e5990104b25b99f33130b99aa026
Nick <nick.russell@bristol.ac.uk>
2015-03-26 23:28:00 +0000

    Final draft
    I'm passing this on now. I hope (probably in vain) that I never
    have to come back to it. It's drained far too much of my time
    already.

--- figures/cascade.pyx
+++ figures/cascade.pyx
@@ -14,11 +14,10 @@ set multiplot
   text "\(r_{2}\)" at 35*fig_m,33*fig_m
   text "\(r_{m-1}\)" at 72*fig_m,13*fig_m
 
-  text "\(\alpha_{0}\)" at 7.5*fig_m,44*fig_m
-
-  text "\(\phi_{1}\)" at 86*fig_m,45*fig_m
-  text "\(\phi_{2}\)" at 86*fig_m,35*fig_m
-  text "\(\phi_{m-1}\)" at 85*fig_m,15*fig_m
+  text "\(\phi_{0}\)" at 86*fig_m,45*fig_m
+  text "\(\phi_{1}\)" at 86*fig_m,35*fig_m
+  text "\(\phi_{m-2}\)" at 85*fig_m,15*fig_m
+  text "\(\phi_{m-1}\)" at 85*fig_m,5*fig_m
 
   # Cartesian labels
   text "\(x_{0}\)" at 98*fig_m,43*fig_m
